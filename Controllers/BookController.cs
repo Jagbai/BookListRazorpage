a9b16fc97bae27a57d9c809cb2ff628cf9bac12e
James Agbai <james.agbai@hotmail.com>
2020-04-05 11:49:54 +0100

    Finished project

---
+++ Controllers/BookController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookListRazor.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.Web.CodeGeneration.Contracts.Messaging;
+
+namespace BookListRazor.Controllers
+{
+    [Route("api/Book")]
+    [ApiController]
+    public class BookController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public BookController(ApplicationDbContext db)
+        {
+            _db = db;
+
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Json(new {data = await _db.Books.ToListAsync() } );
+        }
+
+        [HttpDelete]
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var bookfromDb = await _db.Books.FirstOrDefaultAsync(u => u.Id == id);
+            if (bookfromDb == null)
+            {
+                return Json(new {success = false, Message = "Error while Deleting"});
+
+            }
+
+            _db.Books.Remove(bookfromDb);
+            await _db.SaveChangesAsync();
+            return Json(new {success = true, Message = "Sucessfully Deleted"});
+
+
+        }
+    }
+}
